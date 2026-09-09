f807f45863d6d463573cabb6918dfd7cc87a01ac
akshayshimpi93 <akshayshimpi9390@gmail.com>
2026-09-09 23:47:40 +0530

    DSA in Python by Akkiii

--- Array_&_twopointer2.py
+++ Array_&_twopointer2.py
@@ -45,4 +45,45 @@
 # sen = "A man , a plan , a canal : panama"
 # print(validpalidrom(sen))     # time = O(n)  & space = O(1)
 #--------------------------------------------------------------------------------------------
+'''25.maximum valid sum '''
+#Input: nums = [1,3,5,2,8], k = 2
+# Output: 13
 
+# def maxvalidsum(nums,k):
+#     n = len(nums)
+#     best = float('-inf')
+#     max_left = float('-inf')
+#     i = 0
+
+#     for j in range(k,n):
+#         while i <= j - k:
+#             max_left = max(max_left, nums[i])
+#             i += 1
+#         best = max(best,max_left + nums[j])
+#     return best
+
+# nums = [1,3,5,2,8]
+# k = 2
+# print(maxvalidsum(nums,k))
+# ------------------------------------------------------------------------------------------------
+''' Majority Element '''
+#input= [3,2,3]
+# output = 3
+
+# def majorityElem(nums):
+#     freq = 0
+#     ans = 0
+#     n = len(nums)
+
+#     for i in range(n):
+#         if(freq == 0):
+#             ans = nums[i]
+#             if(ans == nums[i]):
+#                 freq += 1
+#             else:
+#                 freq -= 1
+#     return ans
+
+# nums = [3,2,3]
+# print(majorityElem(nums))
+# -------------------------------------------------------
