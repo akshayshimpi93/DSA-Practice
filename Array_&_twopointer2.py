e5b13d5bff36c71456e91ec2dfc8a4e248baea9a
akshayshimpi93 <akshayshimpi9390@gmail.com>
2026-09-11 21:53:15 +0530

    DSA in Python by Akkiii

--- Array_&_twopointer2.py
+++ Array_&_twopointer2.py
@@ -142,3 +142,101 @@
 
 # matrix = [[1,1,1],[1,0,1],[1,1,1]]
 # print(setZeros(matrix))
+
+
+''' OR ====='''
+# def setZeroes( matrix):
+
+#         m = len(matrix)
+#         n = len(matrix[0])
+
+#         row = [False] * m
+#         col = [False] * n
+
+#         # Mark
+#         for i in range(m):
+#             for j in range(n):
+#                 if matrix[i][j] == 0:
+#                     row[i] = True
+#                     col[j] = True
+
+#         # Update
+#         for i in range(m):
+#             for j in range(n):
+#                 if row[i] or col[j]:
+#                     matrix[i][j] = 0
+
+#  matrix = [[1,1,1],[1,0,1],[1,1,1]]
+# # print(setZeros(matrix))
+
+# ---------------------------------------------------------------------------------------------
+''' Spiral matrix'''
+#Input = [[1,2,3],[4,5,6],[7,8,9]]
+# output = [1,2,3,6,9,8,7,4,5] 
+# def Spiralmatrix(matrix):
+#     m = len(matrix)
+#     n = len(matrix[0])
+
+#     srow = 0
+#     scol = 0
+#     erow = m - 1
+#     ecol = n - 1
+
+#     answer = []
+
+#     while srow <= erow and scol <= ecol:
+
+#         # Top
+#         for j in range(scol, ecol + 1):
+#             answer.append(matrix[srow][j])
+
+#         # Right
+#         for i in range(srow + 1, erow + 1):
+#             answer.append(matrix[i][ecol])
+
+#         # Bottom
+#         for j in range(ecol - 1, scol - 1, -1):
+#             if srow == erow:
+#                 break
+#             answer.append(matrix[erow][j])
+
+#         # Left
+#         for i in range(erow - 1, srow, -1):
+#             if scol == ecol:
+#                 break
+#             answer.append(matrix[i][scol])
+
+#         srow += 1
+#         erow -= 1
+#         scol += 1
+#         ecol -= 1
+
+#     return answer
+
+
+# matrix = [[1, 2, 3],
+#           [4, 5, 6],
+#           [7, 8, 9]]
+
+# print(Spiralmatrix(matrix))
+# ----------------------------------------------------------------------------------------------------
+'''27.Rotate image'''
+# Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+# def rotateimage(matrix):
+#     n = len(matrix)
+
+#     for i in range(n):   # transpose
+#         for j in range(i+1,n):
+#             matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+
+
+#     for row in matrix:   # rotate
+#         row.reverse()
+
+
+# matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# rotateimage(matrix)
+# print(matrix)             #time = O(n^2)   and space = O(1)
+# ---------------------------------------------------------------------------------------------------------
