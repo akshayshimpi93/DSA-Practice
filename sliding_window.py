3271eb82e85b2cdda1e7de4854e11c53cb6931ab
akshayshimpi93 <akshayshimpi9390@gmail.com>
2026-09-15 22:18:39 +0530

    DSA in Python by Akkiii

---
+++ sliding_window.py
@@ -0,0 +1,35 @@
+'''💥🥇 SLIFING WINDOW '''
+'''Slinding Window Maximum '''
+#input =  [1,3,-1,-3,5,3,6,7], k = 3 
+#output = [3,3,5,5,6,7]
+
+# from collections import deque
+
+# def max_sliding_window(nums, k):
+#     dq = deque()
+#     result = []
+
+#     for i in range(len(nums)):
+
+#         # Remove indices outside the window
+#         while dq and dq[0] <= i - k:
+#             dq.popleft()
+
+#         # Remove smaller elements from the back
+#         while dq and nums[dq[-1]] <= nums[i]:
+#             dq.pop()
+
+#         # Add current index
+#         dq.append(i)
+
+#         # Start adding maximums after first window
+#         if i >= k - 1:
+#             result.append(nums[dq[0]])
+
+#     return result
+
+
+# nums = [1, 3, -1, -3, 5, 3, 6, 7]
+# k = 3
+
+# print(max_sliding_window(nums, k))
